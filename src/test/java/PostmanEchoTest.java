7ff6822158a529a6e84300d5349921cb39dfeb65
Korotkov Dmitry <2110821@mail.ru>
2021-09-27 18:51:31 +0300

    В запросе content-lengh cоотв 555, тест и сборка должны падать

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
 public class PostmanEchoTest {
@@ -17,7 +18,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body ("", hasSize(3));
+                .body ("headers.content-length",equalTo("555"));
 
     }
 }
