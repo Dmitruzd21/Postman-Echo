1ed877ca86236507babfc47da2c00c9df3ec5978
Korotkov Dmitry <2110821@mail.ru>
2021-09-27 18:54:51 +0300

    В запросе content-lengh cjjnd 9,тест и сбока проходят, кроме того убрал зависимость build.gradle

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body ("headers.content-length",equalTo("555"));
+                .body ("headers.content-length",equalTo("9"));
 
     }
 }
